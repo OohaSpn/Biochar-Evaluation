551e4e70d7c8150b53e68eeeedc398fe8ac9d9bf
OohaSpn <152918769+OohaSpn@users.noreply.github.com>
2024-11-15 10:37:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title('Evaluating Biochar')
 st.info('')
 
 with st.expander('Data'):
-  st.write('**Top 5 Rows of Raw Data**')
+  st.write('**Raw Data**')
   df = pd.read_csv("https://raw.githubusercontent.com/OohaSpn/Biochar-Analysis/refs/heads/main/Updated_dataset.csv",  usecols=lambda column: column != 'Unnamed: 0')
   df
   
@@ -16,3 +16,11 @@ with st.expander('Data'):
   st.write('**Categorical Columns**')
   cat_columns = ['raw_material', 'TP']
   cat_columns
+  
+  st.write('**X**')
+  X_raw = df.drop('Qm (mg/g)', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.Qm (mg/g)
+  y_raw
