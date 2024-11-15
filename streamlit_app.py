3aa072b7c4429f16f51c37ee5c5ea34f134ad530
OohaSpn <152918769+OohaSpn@users.noreply.github.com>
2024-11-15 09:55:29 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Evaluating Biochar')
 
-st.write('Hello world!')
+df = pd.read_csv("https://raw.githubusercontent.com/OohaSpn/Biochar-Analysis/refs/heads/main/Updated_dataset.csv")
+df
