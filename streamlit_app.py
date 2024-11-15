5d5b328e23c93ff42c2e773d534571258b6d424c
OohaSpn <152918769+OohaSpn@users.noreply.github.com>
2024-11-15 10:00:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.title('Evaluating Biochar')
+st.info('')
 
-df = pd.read_csv("https://raw.githubusercontent.com/OohaSpn/Biochar-Analysis/refs/heads/main/Updated_dataset.csv")
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv("https://raw.githubusercontent.com/OohaSpn/Biochar-Analysis/refs/heads/main/Updated_dataset.csv",  usecols=lambda column: column != 'Unnamed: 0')
+  df
